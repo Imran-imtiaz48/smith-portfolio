559bd8f2ff7b6c93a55563aa5dd02d6fb69e8726
Imran Imtiaz <emraan.imtiaz48@gmail.com>
2025-01-29 12:47:32 +0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,45 @@
-'use strict';
-
-
+"use strict";
 
 /**
- * add event on element
+ * Add event listener to single or multiple elements
+ * @param {NodeList | Element} elements - The element(s) to attach the event to
+ * @param {string} type - The event type (e.g., "click")
+ * @param {Function} callback - The function to execute on event
  */
-
-const addEventOnElem = function (elem, type, callback) {
-  if (elem.length > 1) {
-    for (let i = 0; i < elem.length; i++) {
-      elem[i].addEventListener(type, callback);
-    }
+const addEventListenerMulti = (elements, type, callback) => {
+  if (NodeList.prototype.isPrototypeOf(elements)) {
+    elements.forEach(element => element.addEventListener(type, callback));
   } else {
-    elem.addEventListener(type, callback);
+    elements.addEventListener(type, callback);
   }
-}
-
-
+};
 
 /**
- * toggle navbar
+ * Toggle mobile navbar visibility
  */
-
 const navbar = document.querySelector("[data-navbar]");
 const navLinks = document.querySelectorAll("[data-nav-link]");
 const navToggler = document.querySelector("[data-nav-toggler]");
 
-const toggleNavbar = function () {
+const toggleNavbar = () => {
   navbar.classList.toggle("active");
   navToggler.classList.toggle("active");
-}
+};
 
-addEventOnElem(navToggler, "click", toggleNavbar);
-
-const closeNavbar = function () {
+const closeNavbar = () => {
   navbar.classList.remove("active");
   navToggler.classList.remove("active");
-}
+};
 
-addEventOnElem(navLinks, "click", closeNavbar);
-
-
+// Add event listeners
+addEventListenerMulti(navToggler, "click", toggleNavbar);
+addEventListenerMulti(navLinks, "click", closeNavbar);
 
 /**
- * header active
+ * Change header style on scroll
  */
-
 const header = document.querySelector("[data-header]");
 
-window.addEventListener("scroll", function () {
-  if (window.scrollY > 100) {
-    header.classList.add("active");
-  } else {
-    header.classList.remove("active");
-  }
+window.addEventListener("scroll", () => {
+  header.classList.toggle("active", window.scrollY > 100);
 });
